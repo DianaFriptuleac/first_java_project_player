12c083625dbc86e18527c5282ab3fc64cc432554
Diana <dianadorojuc3@gmail.com>
2024-09-27 12:50:45 +0200

    commit metodo per stampare

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,20 @@ public class Main {
         ElementoMultimediale[] elementiPlayer = new ElementoMultimediale[5];
 
         //Creo i miei elementi
-        for(int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             System.out.println("Inserisci l'elemento del player: 1-Audio; 2-Video; 3-Immagine: ");
             int elementoP = scanner.nextInt();
             scanner.nextLine();
             System.out.println("Inserisci il titolo del elemento: ");
             String titolo = scanner.nextLine();
 
-            switch(elementoP) {
+            switch (elementoP) {
                 case 1:
                     System.out.println("Inserisci la durata audio: ");
                     int durataA = scanner.nextInt();
                     System.out.println("Inserisci il volume audio: ");
                     int volAudio = scanner.nextInt();
-                    elementiPlayer[i] = new Audio(titolo,durataA,volAudio);
+                    elementiPlayer[i] = new Audio(titolo, durataA, volAudio);
                     break;
 
                 case 2:
@@ -30,19 +30,35 @@ public class Main {
                     int volVideo = scanner.nextInt();
                     System.out.println("Inserisci la luminosità del video: ");
                     int lumVideo = scanner.nextInt();
-                    elementiPlayer[i] = new Video(titolo,durataAV,volVideo,lumVideo);
+                    elementiPlayer[i] = new Video(titolo, durataAV, volVideo, lumVideo);
                     break;
 
                 case 3:
                     System.out.println("Inserisci la luminosità dell'immagine: ");
                     int lumImg = scanner.nextInt();
-                    elementiPlayer[i] = new Immagine(titolo,lumImg);
+                    elementiPlayer[i] = new Immagine(titolo, lumImg);
                     break;
 
                 default:
                     System.out.println("Errore! Il numero inserito non corrisponde a nessun elemento! ");
-
+                    i--;
             }
         }
-    }
-}
+
+        //Esecuzione dei metodi
+
+        for (int choice = -1; choice != 0; ) {
+            System.out.println("Scegli un numero da 1 a 5 per andare avanti oppure 0 per uscire: ");
+            choice = scanner.nextInt();
+            if (choice > 0 && choice <= 5) {
+                ElementoMultimediale elemento = elementiPlayer[choice - 1];
+                if (elemento instanceof Immagine) {
+                    ((Immagine) elemento).show();
+                } else if (elemento instanceof Riproduci) {
+                    ((Riproduci) elemento).play();
+                }
+            }
+        }
+        System.out.println("Esecuzione finita!");
+        scanner.close();
+    }}
